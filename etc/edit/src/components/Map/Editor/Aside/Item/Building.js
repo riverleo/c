f6529de7083d7f16b67794b0053741bc8ac276ec
riverleo <iam.riverleo@gmail.com>
2018-08-20 10:41:10 +0900

    아이템 삭제 시 확인 창 추가

--- etc/edit/src/components/Map/Editor/Aside/Item/Building.js
+++ etc/edit/src/components/Map/Editor/Aside/Item/Building.js
@@ -1,3 +1,5 @@
+/* global window */
+
 import _ from 'lodash';
 import axios from 'axios';
 import { fromJS } from 'immutable';
@@ -38,6 +40,7 @@ class Building extends Component {
     const index = _.findIndex(aside[types.BUILDING], i => i.id === data.id);
 
     if (index === -1) { return; }
+    if (!window.confirm('정말로 삭제하시겠습니까?')) { return; }
 
     const items = fromJS(aside[types.BUILDING]).delete(index).toJS();
 
--- etc/edit/src/components/Map/List/index.js
+++ etc/edit/src/components/Map/List/index.js
@@ -1,3 +1,5 @@
+/* global window */
+
 import _ from 'lodash';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -89,6 +91,7 @@ class List extends Component {
     const index = _.findIndex(list.maps, i => i.id === id);
 
     if (index === -1) { return; }
+    if (!window.confirm('정말로 삭제하시겠습니까?')) { return; }
 
     const maps = fromJS(list.maps).delete(index).toJS();
 
