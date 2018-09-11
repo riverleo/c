9eca2a9fc82ae09680fbd2f87b0d3b06a625d794
riverleo <iam.riverleo@gmail.com>
2018-09-11 19:13:24 +0900

    스프라이트 삭제 API 작성 중

--- etc/edit/lib/chop/index.js
+++ etc/edit/lib/chop/index.js
@@ -9,43 +9,12 @@ module.exports = async (buffer, chopWidth, chopHeight) => {
     width,
     height,
   } = image.bitmap;
-  const hashes = [];
-  const alphas = [];
-  const croppeds = [];
   const choppable = getChoppable(width, height, chopWidth, chopHeight);
   const chops = await Promise.map(choppable, ({
     x,
     y,
   }) => {
     const cropped = image.clone().crop(x, y, chopWidth, chopHeight);
-    const hash = cropped.hash();
-
-    let alpha;
-
-    cropped.scan(0, 0, cropped.bitmap.width, cropped.bitmap.height, (scanX, scanY, scanIndex) => {
-      if (!_.isNil(alpha) && alpha !== 0) { return; }
-
-      alpha = cropped.bitmap.data[scanIndex + 3];
-    });
-
-    let same;
-
-    _.forEach(croppeds, (c) => {
-      const diff = Jimp.diff(c, cropped);
-
-      if (diff.percent < 0.15) {
-        same = true;
-      }
-    });
-
-    console.log({ same, alpha, hash });
-    if (same) { return undefined; }
-    if (alpha <= 10) { return undefined; }
-    if (_.includes(hashes, hash)) { return undefined; }
-
-    hashes.push(hash);
-    alphas.push(alpha);
-    croppeds.push(cropped);
 
     return cropped.getBufferAsync(Jimp.MIME_PNG);
   });
--- etc/edit/server/routes/terrains/:id/sprite/:chop.js
+++ etc/edit/server/routes/terrains/:id/sprite/:chop.js
@@ -10,6 +10,22 @@ module.exports = async (req, res) => {
   const terrain = db.objects('Terrain').find(o => o.id === id);
   const sprite = _.get(terrain, ['chops', chop]);
 
+  if (req.method === 'DELETE') {
+    db.write(() => {
+      if (!_.isNil(terrain)) {
+        console.log({ chops: terrain.chops, chop });
+        console.log({ removed: _.remove(terrain.chops, (__, index) => {
+          console.log(__, index);
+          return index === _.toInteger(chop);
+        }) });
+      }
+
+      send(res, 202);
+    });
+
+    return;
+  }
+
   if (_.isNil(sprite)) {
     send(res, 404);
 
